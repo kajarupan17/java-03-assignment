b7ff8e4a7933594e45b9f54a2f0b9a922451f78d
kajarupan17 <kajarupan17@.com>
2019-10-04 02:17:53 +0530

    first commit

---
+++ q3/Q3_Combo.java
@@ -0,0 +1,27 @@
+public class Q3_Combo
+{
+    private String key;
+    private int value;
+
+    public Q3_Combo(String key, int value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return key;
+    }
+
+    public String getKey()
+    {
+        return key;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+}
---
+++ q6/Q6_Services.java
@@ -0,0 +1,54 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Q6_Services {
+	
+	
+	Connection conn;
+	Statement stmt;
+	ResultSet rs;
+	
+	public void getConnection() {
+		try{
+			conn = DriverManager.getConnection(url,user,pass);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();	
+		}
+	}
+	
+	public void saveData(String question, String choice, String correct) {
+		String sql = "INSERT INTO Question (question,choices,correct_answer) VALUES ('"+question+"','"+choice+"','"+correct+"')";
+		
+		try {
+			getConnection();
+			stmt = conn.createStatement();
+			stmt.executeUpdate(sql);
+			System.out.println("Success!");
+			stmt.close();
+			conn.close();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public ResultSet getData() {
+		String sql = "SELECT * FROM Question";
+		
+		try {
+			getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			System.out.println("Success!");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+}
